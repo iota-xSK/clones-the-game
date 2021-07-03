93d81c592cffb24120c75afb88d8f3219e5f4468
Juraj <juraj.babic123@gmail.com>
2021-07-03 11:55:28 +0200

    Redid player movement. Now it no longer relies on the fps.

--- templates/Player/Player.gd
+++ templates/Player/Player.gd
@@ -1,22 +1,23 @@
 extends KinematicBody2D
 
-var run_speed = 130
-var acceleration = 0.9
-var jump_speed = -300
-var gravity = 1300
-var friction = 0.5
-var land_speed = 600
-var land_acceleration = 0.9
-var land = false
-
-var velocity = Vector2()
-var jumping = false
-
-var floor_too_far = true
-var jump_when_down = false
-
-var outside_velocity = Vector2()
-
+#export (int, 0, 200) var push = 100
+#var run_speed = 130
+#var acceleration = 0.9
+#var jump_speed = -300
+#var gravity = 1300
+#var friction = 0.5
+#var land_speed = 600
+#var land_acceleration = 0.9
+#var land = false
+#
+#var velocity = Vector2()
+#var jumping = false
+#
+#var floor_too_far = true
+#var jump_when_down = false
+#
+#var outside_velocity = Vector2()
+#
 signal on_screen_exited
 
 
@@ -24,48 +25,93 @@ func _init():
 	add_to_group("player_group")
 	
 
-func _physics_process(delta):
-	var right = Input.is_action_pressed("move_right")
-	var left = Input.is_action_pressed("move_left")
-	var jump = Input.is_action_pressed("move_jump")
-	var land_input = Input.is_action_just_pressed("move_down")
-	velocity.y += gravity * delta
-	if jump:
-		if is_on_floor():
-			velocity.y = jump_speed
-		else:
-			floor_too_far = false
-			jump_when_down = true
-			$Timer.start()
-	if land_input and (not land):
-		land = true
-	if land:
-		velocity.y = lerp(velocity.y, land_speed, land_acceleration)
-		#velocity.y = land_speed
-	if is_on_floor():
-		land = false
-		
-	if (not floor_too_far) and jump_when_down and is_on_floor():
-		velocity.y = jump_speed
-		jump_when_down = false
-	#print(velocity.y)
-	if right:
-		velocity.x += lerp(velocity.x, run_speed, acceleration)
-		#velocity.x = clamp(velocity.x + acceleration * delta, -run_speed, run_speed)
-	if left:
-		velocity.x += lerp(velocity.x, -run_speed, acceleration)
-		#velocity.x = clamp(velocity.x - acceleration * delta, -run_speed, run_speed)
-	velocity.x = lerp(velocity.x, 0, friction)
-	velocity += outside_velocity
-	velocity = move_and_slide(velocity, Vector2(0, -1))
-	outside_velocity = Vector2(0, 0)
+#func _physics_process(delta):
+#	var right = Input.is_action_pressed("move_right")
+#	var left = Input.is_action_pressed("move_left")
+#	var jump = Input.is_action_just_pressed("move_jump")
+#	var land_input = Input.is_action_just_pressed("move_down")
+#
+#
+#	#velocity.y += gravity
+#
+#
+#	velocity.y += gravity * delta
+#	if jump:
+#		if is_on_floor():
+#			velocity.y = jump_speed
+#		else:
+#			floor_too_far = false
+#			jump_when_down = true
+#			$Timer.start()
+#	if land_input and (not land):
+#		land = true
+#	if land:
+#		velocity.y = lerp(velocity.y, land_speed, land_acceleration)
+#		#velocity.y = land_speed
+#	if is_on_floor():
+#		land = false
+#
+#	if (not floor_too_far) and jump_when_down and is_on_floor():
+#		velocity.y = jump_speed
+#		jump_when_down = false
+#	#print(velocity.y)
+#	if right:
+#		velocity.x += lerp(velocity.x, run_speed, acceleration)
+#		#velocity.x = clamp(velocity.x + acceleration * delta, -run_speed, run_speed)
+#	if left:
+#		velocity.x += lerp(velocity.x, -run_speed, acceleration)
+#		#velocity.x = clamp(velocity.x - acceleration * delta, -run_speed, run_speed)
+#	velocity.x = lerp(velocity.x, 0, friction)
+#	velocity += outside_velocity
+#	velocity = move_and_slide(velocity, Vector2(0, -1), false, 4, PI/4, false)
+#
+#	for index in get_slide_count():
+#		var collision = get_slide_collision(index)
+#		if collision.collider.is_in_group("bodies"):
+#			collision.collider.apply_central_impulse(-collision.normal * push)
+#	outside_velocity = Vector2(0, 0)
+#
+#
+#func _on_Timer_timeout():
+#	floor_too_far = true
+#
 
-
-func _on_Timer_timeout():
-	floor_too_far = true
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 	#print("freed")
 	emit_signal("on_screen_exited")
 
+
+
+
+export (int) var speed = 130
+export (int) var jump_speed = -300
+export (int) var gravity = 1300
+export (float, 0, 1.0) var friction = 0.6
+export (float, 0, 1.0) var acceleration = 0.9
+var outside_velocity = Vector2(0, 0)
+
+var velocity = Vector2.ZERO
+
+func get_input():
+	var dir = 0
+	if Input.is_action_pressed("move_right"):
+		dir += 1
+	if Input.is_action_pressed("move_left"):
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+
+
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	velocity += outside_velocity
+	velocity = move_and_slide(velocity, Vector2.UP)
+	outside_velocity = Vector2(0, 0)
+	if Input.is_action_just_pressed("move_jump"):
+		if is_on_floor():
+			velocity.y = jump_speed
